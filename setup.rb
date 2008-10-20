5101783acaa3e42b81b928da3bc412bd7a050923
Jordi Bunster <jordi@bunster.org>
2008-10-20 14:34:47 -0400

    Ooopsie. Without this, it doesn't work as a Gem at all.

--- setup.rb
+++ setup.rb
@@ -1,7 +1,6 @@
 PROJECT_DIR = File.expand_path(File.dirname(__FILE__)).chomp('/')
 
-# Include XMLObject
-$:.unshift File.join(PROJECT_DIR, 'lib', 'xml-object')
+# Include XMLObject from here, not from installed gems:
 $:.unshift File.join(PROJECT_DIR, 'lib')
 require 'xml-object'
 
