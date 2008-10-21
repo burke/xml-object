f0b2000b960d682737c6a417a944d4afd1252723
Jordi Bunster <jordi@bunster.org>
2008-10-21 19:21:22 -0400

    Not really a big deal

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,24 +13,11 @@ require 'digest/md5'
 require 'stringio'
 
 begin_require_rescue 'rubygems',      'to load additional gems'
-begin_require_rescue 'redgreen',      'to get color output'
-begin_require_rescue 'ruby-prof',     'to get profiling information'
-begin_require_rescue 'ruby-debug',    'to use the debugger during tests'
-begin_require_rescue 'activesupport', 'to test auto array pluralization'
+begin_require_rescue 'redgreen',      'to get color output on tests'
+begin_require_rescue 'activesupport', 'to test proper array pluralization'
 begin_require_rescue 'hpricot',       'to test the Hpricot adapter'
+begin_require_rescue 'ruby-debug'
 
-if defined?(JRUBY_VERSION)
-  begin_require_rescue 'jrexml', 'to test the JREXML adapter'
-else
+unless defined?(JRUBY_VERSION)
   begin_require_rescue 'libxml', 'to test the LibXML adapter'
-end
-
-{ :lorem      => '61cd24e2959669c3719fbebf6c948cd3',
-  :characters => 'cdcbd9b89b261487fa98c11d856f50fe',
-  :plurals    => '1c22f96d00bc2277cece4d93154ad974',
-  :recipe     => '6087ab42049273d123d473093b04ab12' }.each do |sample, md5|
-
-  unless Digest::MD5.hexdigest(open_sample_xml(sample).read) == md5
-    puts "Warning: Sample file #{sample.to_s}.xml doesn't match expected MD5"
-  end
 end
