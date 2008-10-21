12a4ed1688cb51696a191b4822fc9ceb2e0bafdb
Jordi Bunster <jordi@bunster.org>
2008-10-21 13:30:19 -0400

    Target dispatching is really only relevant to CollectionProxy#method_missing

--- lib/xml-object/collection_proxy.rb
+++ lib/xml-object/collection_proxy.rb
@@ -8,7 +8,15 @@ class XMLObject::CollectionProxy < XMLObject::BlankishSlate # :nodoc:
   def method_missing(m, *a, &b) # :nodoc:
     dispatched = __question_dispatch(m, *a, &b)
     dispatched = __dot_notation_dispatch(m, *a, &b) if dispatched.nil?
-    dispatched = __target_dispatch(m, *a, &b)       if dispatched.nil?
+
+    if dispatched.nil? && @__target.respond_to?(m)
+      dispatched = @__target.__send__(m, *a, &b)
+
+      unless dispatched.nil?
+        instance_eval %{ def #{m}(*a, &b) @__target.#{m}(*a, &b); end }
+      end
+    end
+
     dispatched
   end
 end
